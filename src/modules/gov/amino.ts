661c6acec7a6a05ef6709b8a321faaaf58c3f724
Tan Do (Alex) <tan.do@sharering.network>
2021-12-23 16:44:02 +0700

    Gov msgs updates

--- src/modules/gov/amino.ts
+++ src/modules/gov/amino.ts
@@ -9,6 +9,7 @@ import {MsgDeposit, MsgSubmitProposal, MsgVote} from "../../codec/cosmos/gov/v1b
 import {TextProposal} from "../../codec/cosmos/gov/v1beta1/gov";
 import {AminoConverter} from "../../amino/types";
 import Long from "long";
+import { assert, assertDefinedAndNotNull, isNonNullObject } from "@cosmjs/utils";
 
 export {VoteOption};
 
@@ -37,7 +38,7 @@ export function isMsgTextProposalEncodeObject(encodeObject: EncodeObject): encod
 export interface AminoMsgSubmitProposal extends AminoMsg {
   readonly type: "cosmos-sdk/MsgSubmitProposal";
   readonly value: {
-    readonly content?: {
+    readonly content: {
       readonly type: "cosmos-sdk/TextProposal";
       readonly value: {
         readonly title: string;
@@ -67,7 +68,7 @@ export function isMsgSubmitProposalEncodeObject(encodeObject: EncodeObject): enc
 export interface AminoMsgVote extends AminoMsg {
   readonly type: "cosmos-sdk/MsgVote";
   readonly value: {
-    readonly proposal_id: number;
+    readonly proposal_id: string;
     /** Bech32 account address */
     readonly voter: string;
     readonly option: VoteOption;
@@ -91,7 +92,7 @@ export function isMsgVoteEncodeObject(encodeObject: EncodeObject): encodeObject 
 export interface AminoMsgDeposit extends AminoMsg {
   readonly type: "cosmos-sdk/MsgDeposit";
   readonly value: {
-    readonly proposal_id: number;
+    readonly proposal_id: string;
     /** Bech32 account address */
     readonly depositor: string;
     readonly amount: readonly Coin[];
@@ -115,42 +116,69 @@ export function createAminoTypes(prefix: string): Record<string, AminoConverter>
   return {
     "/cosmos.gov.v1beta1.MsgSubmitProposal": {
       aminoType: "cosmos-sdk/MsgSubmitProposal",
-      toAmino: ({initialDeposit, proposer, content}: MsgSubmitProposal): AminoMsgSubmitProposal["value"] => ({
-        initial_deposit: [...initialDeposit],
-        proposer,
-        content: content
-          ? ((content): any => {
-              const json: any = Any.toJSON(content);
-              return {
-                type: "cosmos-sdk/TextProposal",
-                value: {
-                  title: json.value.title,
-                  description: json.value.description
-                }
-              };
-            })(content)
-          : undefined
-      }),
-      fromAmino: ({initial_deposit, proposer, content}: AminoMsgSubmitProposal["value"]): MsgSubmitProposal => ({
-        initialDeposit: [...initial_deposit],
-        proposer,
-        content: content
-          ? Any.fromJSON({
+      toAmino: ({initialDeposit, proposer, content}: MsgSubmitProposal): AminoMsgSubmitProposal["value"] => {
+        assertDefinedAndNotNull(content);
+        let proposal: any;
+        switch (content.typeUrl) {
+          case "/cosmos.gov.v1beta1.TextProposal": {
+            const textProposal = TextProposal.decode(content.value);
+            proposal = {
+              type: "cosmos-sdk/TextProposal",
+              value: {
+                description: textProposal.description,
+                title: textProposal.title,
+              },
+            };
+            break;
+          }
+          default:
+            throw new Error(`Unsupported proposal type: '${content.typeUrl}'`);
+        }
+        return {
+          initial_deposit: initialDeposit,
+          proposer: proposer,
+          content: proposal,
+        };
+      },
+      fromAmino: ({initial_deposit, proposer, content}: AminoMsgSubmitProposal["value"]): MsgSubmitProposal => {
+        let any_content: Any;
+        switch (content.type) {
+          case "cosmos-sdk/TextProposal": {
+            const { value } = content;
+            assert(isNonNullObject(value));
+            const { title, description } = value as any;
+            assert(typeof title === "string");
+            assert(typeof description === "string");
+            any_content = Any.fromPartial({
               typeUrl: "/cosmos.gov.v1beta1.TextProposal",
-              value: {...content.value}
-            })
-          : undefined
-      })
+              value: TextProposal.encode(
+                TextProposal.fromPartial({
+                  title: title,
+                  description: description,
+                }),
+              ).finish(),
+            });
+            break;
+          }
+          default:
+            throw new Error(`Unsupported proposal type: '${content.type}'`);
+        }
+        return {
+          initialDeposit: Array.from(initial_deposit),
+          proposer: proposer,
+          content: any_content,
+        };
+      }
     },
     "/cosmos.gov.v1beta1.MsgVote": {
       aminoType: "cosmos-sdk/MsgVote",
       toAmino: ({proposalId, voter, option}: MsgVote): AminoMsgVote["value"] => ({
-        proposal_id: proposalId.toNumber(),
+        proposal_id: proposalId.toString(),
         voter,
         option
       }),
       fromAmino: ({proposal_id, voter, option}: AminoMsgVote["value"]): MsgVote => ({
-        proposalId: Long.fromNumber(proposal_id),
+        proposalId: Long.fromString(proposal_id),
         voter,
         option
       })
@@ -158,12 +186,12 @@ export function createAminoTypes(prefix: string): Record<string, AminoConverter>
     "/cosmos.gov.v1beta1.MsgDeposit": {
       aminoType: "cosmos-sdk/MsgDeposit",
       toAmino: ({proposalId, depositor, amount}: MsgDeposit): AminoMsgDeposit["value"] => ({
-        proposal_id: proposalId.toNumber(),
+        proposal_id: proposalId.toString(),
         depositor,
         amount: [...amount]
       }),
       fromAmino: ({proposal_id, depositor, amount}: AminoMsgDeposit["value"]): MsgDeposit => ({
-        proposalId: Long.fromNumber(proposal_id),
+        proposalId: Long.fromString(proposal_id),
         depositor,
         amount: [...amount]
       })
